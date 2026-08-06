8046ab04958e1e2e4747f1ae80fdcbeb7de3a8fd
João P <joaopaulodealmeidapereira1@gmail.com>
2026-08-05 23:44:29 -0300

    inicio de um menu

--- src/Biblioteca/BibliotecaApp.java
+++ src/Biblioteca/BibliotecaApp.java
@@ -10,14 +10,39 @@ public class BibliotecaApp {
         Livro lv3 = new Livro ("Racionais Mc's", "Racionais Mc's", false);
 
 
-           System.out.println("\n--- BIBLIOTECA ---");
-           lv1.exibirFicha(1);
-           lv2.exibirFicha(2);
-           lv3.exibirFicha(3);
-           System.out.println("Total de Livros: " + Livro.getTotalLivros());
-           System.out.println("4 - Sair");
-           System.out.print("\nEscolha um Livro ou 4 para Sair: ");
+        System.out.println("\n--- BIBLIOTECA ---");
+        lv1.exibirFicha(1);
+        lv2.exibirFicha(2);
+        lv3.exibirFicha(3);
+        System.out.println("Total de Livros: " + Livro.getTotalLivros());
+        System.out.println("4 - Sair");
+        System.out.print("\nEscolha um Livro ou 4 para Sair: ");
+        int oplivro = sc.nextInt();
 
+        System.out.println("\n--- BIBLIOTECA ---");
+        if (oplivro == 1){
+            System.out.println("Opção escolhida");
+            lv1.exibirFicha(1);
+            System.out.println("\n");
+        }
+        else if (oplivro == 2){
+            System.out.println("Opção escolhida");
+            lv2.exibirFicha(2);
+            System.out.println("\n");
+        }
+        else if (oplivro == 3){
+            System.out.println("Opção escolhida");
+            lv3.exibirFicha(3);
+            System.out.println("\n");
+        }
+        System.out.println("1 - Devolver Livro");
+        System.out.println("2 - Emprestar Livro");
+        System.out.print("\nEscolha uma opção: ");
+        int opcase = sc.nextInt();
 
+        switch (opcase){
+            case 1:
+                System.out.println("");
+        }
     }
 }
